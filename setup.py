fcf361fde20bc1c058e7228e1ce130914a1e7dd6
Parag Patel <parag.patel.one@gmail.com>
2014-02-19 09:29:43 -0500

    Added Setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='goimport',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Parag Patel',
+    author_email='parag.patel.one@gmail.com',
+    description=''
+)
